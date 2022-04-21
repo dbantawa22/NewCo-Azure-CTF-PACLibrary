3dbdaea38bbdfa4d1fb762ec4db1e3bcfe58d7dd
Chandu Atluri <chandu.atluri@avanade.com>
2022-04-21 16:12:46 -0400

    updated diagnostic settings file

--- PAC_ARM_Template_Libarary_development/run.sh
+++ PAC_ARM_Template_Libarary_development/run.sh
@@ -24,28 +24,34 @@ connect_deployment()
     echo '------------------- Policy Sets Definition --------------------'
 
     echo $DEPLOYMENT_NAME
-    # az deployment sub create --name $DEPLOYMENT_NAME --location $LOCATION --template-uri $AZ_PAC_LIBRARY_PATH"BICEP-azure-security-policies-subscription/main"
-    
-    
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./DENY-PublicIpAddressPolicyAssignment.json --name policy-Deployment-11
-    
-    # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policies.json --name policy-Deployment-1
+    az deployment sub create --name $DEPLOYMENT_NAME --location $LOCATION --template-uri $AZ_PAC_LIBRARY_PATH"BICEP-azure-security-policies-subscription/main"
+        
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policies.json --name policy-Deployment-1
 
-    # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_denyIP.json --name policy-Deployment-2
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_denyIP.json --name policy-Deployment-2
   
-    # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./DENY-PublicEndpointsPolicySetDefinition.json --name policy-Deployment-3
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./DENY-PublicEndpointsPolicySetDefinition.json --name policy-Deployment-3
 
-    # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_keyvault.json --name policy-Deployment-4
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_keyvault.json --name policy-Deployment-4
     
-    # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_enforce_AG_WAF.json --name policy-Deployment-5
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_enforce_AG_WAF.json --name policy-Deployment-5
 
-    # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_ipforwarding.json --name policy-Deployment-6
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_ipforwarding.json --name policy-Deployment-6
     
-    # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./DINE-PrivateDNSZonesPolicySetDefinition.json --name policy-Deployment-7
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./DINE-PrivateDNSZonesPolicySetDefinition.json --name policy-Deployment-7
 
-    # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_network_traffic.json --name policy-Deployment-8
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_network_traffic.json --name policy-Deployment-8
 
-    # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_ddos.json --name policy-Deployment-9
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_ddos.json --name policy-Deployment-9
+   
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_diagnostic_settings.json.json --name policy-Deployment-10
+
+    
+ 
+    echo '------------------- Policy Sets Assignments --------------------'
+
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./DENY-PublicIpAddressPolicyAssignment.json --name policy-set-assignment-deployment-1
+
 
 }
 
