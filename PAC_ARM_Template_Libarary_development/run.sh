eab0898de9227726670dc18bbd3386a8d990ffa2
Chandu Atluri <chandu.atluri@avanade.com>
2022-04-21 11:20:55 -0400

    Added Dine

--- PAC_ARM_Template_Libarary_development/run.sh
+++ PAC_ARM_Template_Libarary_development/run.sh
@@ -36,7 +36,9 @@ connect_deployment()
     
     az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_enforce_AG_WAF.json --name policy-Deployment-5
 
-        az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_ipforwarding.json --name policy-Deployment-6
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_ipforwarding.json --name policy-Deployment-6
+    
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./DINE-PrivateDNSZonesPolicySetDefinition.json --name policy-Deployment-7
 
 
 }
