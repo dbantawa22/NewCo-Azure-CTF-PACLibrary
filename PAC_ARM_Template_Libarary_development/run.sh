69026622e3f8ce3c2995e6dfcb9f332e441e6043
Chandu Atluri <chandu.atluri@avanade.com>
2022-04-22 11:33:27 -0400

    add log json

--- PAC_ARM_Template_Libarary_development/run.sh
+++ PAC_ARM_Template_Libarary_development/run.sh
@@ -27,6 +27,8 @@ connect_deployment()
     # az deployment sub create --name $DEPLOYMENT_NAME --location $LOCATION --template-uri $AZ_PAC_LIBRARY_PATH"BICEP-azure-security-policies-subscription/main"
 
     az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_diagnostic_settings.json --name policy-Deployment-10
+    
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policyset_log_storage.json --name policy-Deployment-10
 
     # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file ./policies.json --name policy-Deployment-1
 
