c83652af4910a6343be26b3fc31a2a58c51f8cd4
Chandu Atluri <chandu.atluri@avanade.com>
2022-04-22 13:37:27 -0400

    updated .sh

--- PAC_ARM_Template_Libarary_development/run.sh
+++ PAC_ARM_Template_Libarary_development/run.sh
@@ -26,7 +26,7 @@ connect_deployment()
     echo $DEPLOYMENT_NAME
     # az deployment sub create --name $DEPLOYMENT_NAME --location $LOCATION --template-uri $AZ_PAC_LIBRARY_PATH"BICEP-azure-security-policies-subscription/main"
 
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file  ./allowed_locations.json --name policy-Deployment-16
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file  ./policyset_allowed_locations.json --name policy-Deployment-16
 
     #     az deployment mg create --location uksouth --management-group-id MGMT_ROOT --template-file  ./policyset_vm_backup.json --name policy-Deployment-13
 
